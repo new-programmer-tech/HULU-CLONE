dac4287b72ab20109516a284b2f793a4f21c730d
harshal <harshalogale24@gmail.com>
2021-09-15 02:37:35 +0530

    js added

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 'use strict';
 
+const open = document.querySelector('.modal');
+const close =document.querySelector('.close');
+const login =document.querySelector('.login-btn');
+
+
+login.addEventListener('click',openLogin);
+close.addEventListener('click',closeLogin);
+
+
+function openLogin()
+{
+    open.style.display = "block";
+}
+
+
+function closeLogin()
+{
+    open.style.display = "none";
+}
+
 
 
